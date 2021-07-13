b8d2fad37cceba5c75a9e3063563e265add691cc
Graham Bull <graham.bull@gmail.com>
2021-07-13 10:01:14 +0100

    feat: commander

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,20 @@
-#!/usr/bin/env node
+import { program } from 'commander';
 
-import fs from 'fs';
-import path from 'path';
-import { parse } from 'whats-the-release';
+import { subfinder } from './subfinder';
 
-import { bestMatch } from './utils/bestMatch';
-import { filterResultsByEpisode } from './utils/filterResultsByEpisode';
-import {
-  parseSearchPage,
-  parseSubtitleDownloadLink,
-  parseSubtitlesPage,
-} from './utils/parse';
-import { seasonToOrdinal } from './utils/seasonToOrdinal';
-import { subSceneApi } from './utils/subSceneApi';
-import { unzipSubtitles } from './utils/unpackSubtitles';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const VERSION = require('../package.json').version;
 
-export async function subFinder(file: string, dir: string): Promise<void> {
-  if (!fs.existsSync(path.join(dir, file))) {
-    throw new Error('Unable to locate file.');
-  }
-
-  // I want to display version info
-  console.log('Subfinder');
-
-  const release = parse(file);
-
-  const searchQuery = release.name;
-  let searchQueryMatch = release.name;
-
-  if (release.type === 'Movie') {
-    const { name, year } = release;
-    searchQueryMatch = year ? `${name} (${year})` : name;
-  }
-  if (release.type === 'Show') {
-    const { name, season } = release;
-    searchQueryMatch = `${name} - ${seasonToOrdinal(season)} Season`;
-  }
-
-  const searchPage = await subSceneApi.search(searchQuery);
-  const parsedSearchPage = parseSearchPage(searchPage);
-  const searchResult = bestMatch(parsedSearchPage, searchQueryMatch);
-
-  console.log('Selected release:', searchResult.title);
-
-  const subtitlesPage = await subSceneApi.getPage(searchResult.link);
-  let parsedSubtitlesPage = parseSubtitlesPage(subtitlesPage);
-  if (release.type === 'Show') {
-    parsedSubtitlesPage = filterResultsByEpisode(
-      parsedSubtitlesPage,
-      release.episode
-    );
-  }
-  const subtitlesResult = bestMatch(parsedSubtitlesPage, file);
-
-  console.log('Selected subtitles:', subtitlesResult.title);
-
-  const downloadPage = await subSceneApi.getPage(subtitlesResult.link);
-  const downloadUrl = parseSubtitleDownloadLink(downloadPage);
-
-  const zipFile = await subSceneApi.getZipFile(downloadUrl);
-
-  const destFile = release.container
-    ? path.join(dir, `${file.slice(0, file.length - 4)}.srt`)
-    : path.join(dir, file + '.srt');
-
-  unzipSubtitles(zipFile, destFile);
-
-  console.log('Complete');
-}
-
-subFinder(process.argv[2], process.cwd())
-  .then()
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+program
+  .version(VERSION)
+  .argument('<file>', 'file to download subtitles for')
+  .action(async (filename: string) => {
+    try {
+      await subfinder(filename, process.cwd());
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  })
+  .description('A CLI app for downloading subtitles.')
+  .parse(process.argv);
---
+++ src/subfinder.ts
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'whats-the-release';
+
+import { bestMatch } from './utils/bestMatch';
+import { filterResultsByEpisode } from './utils/filterResultsByEpisode';
+import {
+  parseSearchPage,
+  parseSubtitleDownloadLink,
+  parseSubtitlesPage,
+} from './utils/parse';
+import { seasonToOrdinal } from './utils/seasonToOrdinal';
+import { subSceneApi } from './utils/subSceneApi';
+import { unzipSubtitles } from './utils/unpackSubtitles';
+
+export async function subfinder(file: string, dir: string): Promise<void> {
+  if (!fs.existsSync(path.join(dir, file))) {
+    throw new Error('Unable to locate file.');
+  }
+
+  // I want to display version info
+  console.log('Subfinder');
+
+  const release = parse(file);
+
+  const searchQuery = release.name;
+  let searchQueryMatch = release.name;
+
+  if (release.type === 'Movie') {
+    const { name, year } = release;
+    searchQueryMatch = year ? `${name} (${year})` : name;
+  }
+  if (release.type === 'Show') {
+    const { name, season } = release;
+    searchQueryMatch = `${name} - ${seasonToOrdinal(season)} Season`;
+  }
+
+  const searchPage = await subSceneApi.search(searchQuery);
+  const parsedSearchPage = parseSearchPage(searchPage);
+  const searchResult = bestMatch(parsedSearchPage, searchQueryMatch);
+
+  console.log('Selected release:', searchResult.title);
+
+  const subtitlesPage = await subSceneApi.getPage(searchResult.link);
+  let parsedSubtitlesPage = parseSubtitlesPage(subtitlesPage);
+  if (release.type === 'Show') {
+    parsedSubtitlesPage = filterResultsByEpisode(
+      parsedSubtitlesPage,
+      release.episode
+    );
+  }
+  const subtitlesResult = bestMatch(parsedSubtitlesPage, file);
+
+  console.log('Selected subtitles:', subtitlesResult.title);
+
+  const downloadPage = await subSceneApi.getPage(subtitlesResult.link);
+  const downloadUrl = parseSubtitleDownloadLink(downloadPage);
+
+  const zipFile = await subSceneApi.getZipFile(downloadUrl);
+
+  const destFile = release.container
+    ? path.join(dir, `${file.slice(0, file.length - 4)}.srt`)
+    : path.join(dir, file + '.srt');
+
+  unzipSubtitles(zipFile, destFile);
+
+  console.log('Complete');
+}
