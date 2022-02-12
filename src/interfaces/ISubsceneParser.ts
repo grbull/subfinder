ee2de7f9b56247c4ccbc5a15eaab9025d9ec5dab
Graham Bull <graham.bull@gmail.com>
2022-02-12 16:39:40 +0000

    refactor: missing from PR

--- src/interfaces/ISubsceneParser.ts
+++ src/interfaces/ISubsceneParser.ts
@@ -2,5 +2,5 @@ import { IOption } from './IOption';
 
 export interface ISubsceneParser {
   parseMediaOptions<T extends Element>(elements: T[]): IOption[];
-  parseSubtitleOptions<T extends Element>(elements: T[]): IOption[];
+  parseMediaSubtitleOptions<T extends Element>(elements: T[]): IOption[];
 }
--- src/interfaces/ISubtitleSource.ts
+++ src/interfaces/ISubtitleSource.ts
@@ -5,6 +5,6 @@ import { IOptionRated } from './IOptionRated';
 
 export interface ISubtitleSource {
   searchMediaOptions(release: WtrResult): Promise<IOptionRated[]>;
-  getSubtitleOptions(mediaOption: IOption, release: WtrResult): Promise<IOptionRated[]>;
+  getMediaSubtitleOptions(mediaOption: IOption, release: WtrResult): Promise<IOptionRated[]>;
   downloadSubtitles(subtitleOption: IOption, destinationPath: string): Promise<void>;
 }
