067f75ee12b83ff57f096a4e77b880646c7c6e1e
Graham Bull <graham.bull@gmail.com>
2022-02-12 20:52:05 +0000

    test: subscene api tests

---
+++ src/subscene/__tests__/SubsceneApi.tests.ts
@@ -0,0 +1,47 @@
+import Axios from 'axios';
+
+import { SubSceneApi } from '../SubsceneApi';
+
+describe('SubsceneApi', () => {
+  describe('fetchSearchPage', () => {
+    it('should make an axios request', () => {
+      // Arrange
+      const axios = Axios.create();
+      const axiosRequestMock = jest.spyOn(axios, 'request');
+      axiosRequestMock.mockImplementation((): Promise<string> => Promise.resolve(''));
+      const testQuery = 'test';
+      const sut = new SubSceneApi(axios);
+
+      // Act
+      sut.fetchSearchPage(testQuery);
+
+      // Assert#
+      expect(axiosRequestMock).toHaveBeenCalledTimes(1);
+      expect(axiosRequestMock).toHaveBeenCalledWith({
+        method: 'POST',
+        url: 'https://subscene.com/subtitles/searchbytitle',
+        data: `query=${testQuery}&l=`,
+      });
+    });
+  });
+  describe('fetchPage', () => {
+    it('should make an axios request', () => {
+      // Arrange
+      const axios = Axios.create();
+      const axiosRequestMock = jest.spyOn(axios, 'request');
+      axiosRequestMock.mockImplementation((): Promise<string> => Promise.resolve(''));
+      const sut = new SubSceneApi(axios);
+      const testUrl = '';
+
+      // Act
+      sut.fetchPage(testUrl);
+
+      // Assert#
+      expect(axiosRequestMock).toHaveBeenCalledTimes(1);
+      expect(axiosRequestMock).toHaveBeenCalledWith({
+        method: 'GET',
+        url: testUrl,
+      });
+    });
+  });
+});
