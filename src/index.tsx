c91931eab020dc3566b24761f1f7e3ec334f48cf
Graham Bull <graham.bull@gmail.com>
2023-02-09 22:54:48 +0000

    fix: usage of commander js

--- src/index.tsx
+++ src/index.tsx
@@ -12,35 +12,29 @@ import { Subfinder } from './Subfinder';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const VERSION = require('../package.json').version;
 
-// Our options/flags
 interface CliOptions {
   interactive?: boolean;
 }
 
-function main(relativeFilePath: string, options: CliOptions): void {
-  const filePath = path.join(process.cwd(), relativeFilePath);
-
-  if (!fs.existsSync(filePath)) {
-    console.error('Error: Unable to locate media file.');
-    process.exit(1);
-  }
-
-  // Parse the filename for media info using whats-the-release package
-  const fileName = path.basename(filePath);
-  const release = parse(fileName);
-
-  render(<Subfinder filePath={filePath} isInteractive={options.interactive} release={release} version={VERSION} />);
-}
-
 program
-  // Meta info
+  .name('subfinder')
   .description('A CLI app for downloading subtitles.')
   .version(VERSION)
-  // CLI Options
-  .option('-I, --interactive', 'Interactive Mode', false)
-  // Main argument (filename)
   .argument('<file>', 'file to download subtitles for')
-  // Parse args
-  .parse(process.argv)
-  // Run
-  .action(main);
+  .option('-I, --interactive', 'Interactive Mode', false)
+  .parse();
+
+const options = program.opts() as CliOptions;
+const relativeFilePath = process.argv[2];
+const filePath = path.join(process.cwd(), relativeFilePath);
+
+if (!fs.existsSync(filePath)) {
+  console.error('Error: Unable to locate media file.');
+  process.exit(1);
+}
+
+// Parse the filename for media info using whats-the-release package
+const fileName = path.basename(filePath);
+const release = parse(fileName);
+
+render(<Subfinder filePath={filePath} isInteractive={options.interactive} release={release} version={VERSION} />);
